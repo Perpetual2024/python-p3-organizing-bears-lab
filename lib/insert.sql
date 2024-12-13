dd2d51984ce8e2cbc9b15438dc5d6f7c24056c0e
Perpetual Akinyi <you@example.com>
2024-12-13 12:55:02 +0300

    learned SQL

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+ VALUES 
+ ("Mr. Chocolate", 5, "M", "Brown", "Friendly", TRUE),
+ ("Rowdy", 3, "M", "Black", "Aggressive", TRUE),
+ ("Tabitha", 2, "F", "White", "Playful", TRUE),
+ ("Sergeant Brown", 1, "F", "Brown", "Friendly", TRUE),
+ ("Melissa", 4, "M", "Black", "Aggressive", TRUE),
+ ("Grinch", 6, "F", "White", "Playful", TRUE),
+ ("Wendy", 5, "F", "Brown", "Friendly", TRUE),
+ (NULL, 3, "M", "Black", "Aggressive", TRUE)
+;
